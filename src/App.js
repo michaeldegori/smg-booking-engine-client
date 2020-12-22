a01c33124fc91c8f573b9d9b20f62e60463f7ba9
Michael DeGori <michael.degori@gmail.com>
2020-12-21 19:16:29 -0500

    first

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ const App = (props) => {
   const handleLoginClose = () => setShowLogin(false);
   const handleLoginShow = () => setShowLogin(true);
 
-  const onSignupFormSubmit = (event) => {
+  const onLoginFormSubmit = (event) => {
     event.preventDefault();
     handleSignupClose();
   };
