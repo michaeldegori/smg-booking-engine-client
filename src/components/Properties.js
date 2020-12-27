17231e14d3ed67caadce23269355642173f25318
Michael DeGori <michael.degori@gmail.com>
2020-12-27 15:07:48 -0500

    col fluid

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -3,10 +3,32 @@ import { userContext } from '../contexts/User';
 import Axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../styles/Properties.css';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '100vh',
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
 
 const Properties = (props) => {
   const { user } = useContext(userContext);
   const [properties, setProperties] = useState([]);
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
 
   useEffect(() => {
     Axios.get(`${process.env.REACT_APP_BACKEND}properties`)
@@ -17,7 +39,7 @@ const Properties = (props) => {
   const listProperties = () => {
     return properties?.map((property) => {
       return (
-        <div className="col-md-6">
+        <div className="col">
           <div className="card mb-3">
             <img
               src={property?.photos}
@@ -65,17 +87,33 @@ const Properties = (props) => {
 
   return (
     <section className="properites">
-      <div className="container-xl py-3">
+      <div className="container-fluid py-3">
         <h3 className="text-dark">Our properties</h3>
         <div className="row mt-4">
-          {listProperties()}{' '}
-          {user && (
-            <Link to="/properties/add">
-              <button className="btn btn-primary text-white">
-                Add New Property
-              </button>
-            </Link>
-          )}
+          <div className="col">
+            {listProperties()}{' '}
+            {user && (
+              <Link to="/properties/add">
+                <button className="btn btn-primary text-white">
+                  Add New Property
+                </button>
+              </Link>
+            )}
+          </div>
+          <div className="col-md-5 position-relative">
+            <LoadScript googleMapsApiKey="YOUR_API_KEY">
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+              >
+                {/* Child components, such as markers, info windows, etc. */}
+                <></>
+              </GoogleMap>
+            </LoadScript>
+          </div>
         </div>
       </div>
     </section>
